75afe73ec20b52a05a70473d03bfaa30bc7fe285
vaca22 <wsmslgh@gmail.com>
2022-07-17 17:51:36 +0800

    gfh

--- main.py
+++ main.py
@@ -6,19 +6,20 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 
 def window():
     app = QApplication(sys.argv)
-    win=QMainWindow()
-    win.setGeometry(200,200,300,300)
+    win = QMainWindow()
+    win.setGeometry(200, 200, 300, 300)
     win.setWindowTitle("gaga")
 
-    label=QtWidgets.QLabel(win)
+    label = QtWidgets.QLabel(win)
     label.setText("gaga")
-    label.move(50,50)
+    label.move(50, 50)
     win.show()
     sys.exit(app.exec_())
 
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-#    print_hi('PyCharm')
+    #    print_hi('PyCharm')
     window()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
